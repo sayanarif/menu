c028043003d2facdbf144a60fe03c7f10fecb8c4
User <arif22113@gmail.com>
2018-06-08 06:42:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/user/menu/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.user.menu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.commonmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.aboutid){
+            startActivity(new Intent(this,Main2Activity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
